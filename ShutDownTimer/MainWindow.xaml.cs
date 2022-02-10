f9805f8bef9469074009f9014001b8d5dcda91b1
Joe <joetingle@live.co.uk>
2022-02-10 12:41:26 +0000

    Add project files.

---
+++ ShutDownTimer/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace ShutDownTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string sCurrentTime = "Time String";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            sCurrentTime =  DateTime.Now.ToString("h:mm:ss tt");
+            TimeLabel.Content = sCurrentTime;
+        }
+    }
+}
