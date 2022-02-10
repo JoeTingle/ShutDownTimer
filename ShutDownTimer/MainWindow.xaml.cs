f84b225a44dcf6bfad893c0ab0645950440e06c1
Joe <joetingle@live.co.uk>
2022-02-10 16:09:23 +0000

    - Added .ico for executable - Fully implemented shutdown - Added hour/min textboxes for custom shutdown time - Added cancel button to cancel timer if needed - Window minimises after timer is started

--- ShutDownTimer/MainWindow.xaml.cs
+++ ShutDownTimer/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Timers;
 using System.Windows.Threading;
-using System.Diagnostics;
 
 namespace ShutDownTimer
 {
@@ -24,16 +12,22 @@ namespace ShutDownTimer
     public partial class MainWindow : Window
     {
         string sCurrentTime = "Time String";
+        DispatcherTimer timer = new();
+        DispatcherTimer shutdownTimer = new();
+        TimeSpan _time = new TimeSpan(0,0,0,0);
         public MainWindow()
         {
             InitializeComponent();
 
-            DispatcherTimer timer = new DispatcherTimer();
+            // Timer to update system time
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += UpdateTime;
             timer.Start();
 
-
+            //shutdown
+            _time = new TimeSpan(0,Int32.Parse(HourLabel.Text), Int32.Parse(MinLabel.Text), 0);
+            shutdownTimer.Interval = _time;
+            shutdownTimer.Tick += ExecuteShutdown;
         }
         private void TimeLabel_Loaded(object sender, RoutedEventArgs e)
         {
@@ -45,15 +39,69 @@ namespace ShutDownTimer
         {
             sCurrentTime = DateTime.Now.ToString("h:mm:ss tt");
             TimeLabel.Content = "System Time : " + sCurrentTime;
+            UpdateShutdownTimeIn();
         }
 
-        public void ExecuteShutdown()
+        public void UpdateShutdownTimer()
+        {
+            if (HourLabel != null)
+            {
+                if (MinLabel != null)
+                {
+                    _time = new TimeSpan(0, Int32.Parse(HourLabel.Text), 0, Int32.Parse(MinLabel.Text));
+                    shutdownTimer.Interval = _time;
+                }
+            }
+
+        }
+
+        public void UpdateShutdownTimeIn()
+        {
+            if (_time.TotalMinutes == 0)
+            {
+                ShutdownInTimeLabel.Content = "Press Start Timer To Begin ...";
+            }
+            else
+            {
+                ShutdownInTimeLabel.Content = "System Shutting Down In " + _time.Hours + " Hours " + _time.Minutes + " Minutes";
+            }
+        }
+
+        public void ExecuteShutdown(object sender, EventArgs e)
         {
             var psi = new ProcessStartInfo("shutdown", "/s /t 0");
             psi.CreateNoWindow = true;
             psi.UseShellExecute = false;
             Process.Start(psi);
+
         }
 
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            //Timer to execute shutdown
+            shutdownTimer.Start();
+            btnStart.IsEnabled = false;
+            HourLabel.IsEnabled = false;
+            MinLabel.IsEnabled = false;
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            shutdownTimer.Stop();
+            btnStart.IsEnabled = true;
+            HourLabel.IsEnabled = true;
+            MinLabel.IsEnabled= true;
+        }
+
+        private void HourLabel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateShutdownTimer();
+        }
+
+        private void MinLabel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateShutdownTimer();
+        }
     }
 }
